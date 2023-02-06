c1d1b0ef4548d65fd6a91d6041ec95350d89fcd7
Sheep26 <crabeykiller@gmail.com>
2023-02-06 15:52:05 +1300

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from sheepbot26 import token
+from sheepbot26 import token, get_uptime_sec, get_uptime
 import time
 import datetime
 import socket
@@ -15,10 +15,11 @@ class Server(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>Sheepbot26</title></head>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         # setTimeout(function() {}, 1000);
-        self.wfile.write(bytes("<script>var time = 0; var minutes = 0; var hours = 0; var intervalId = window.setInterval(function(){update_uptime()}, 1000); function update_uptime(){time += 1; if(time == 60){time = 0; minutes += 1; if(minutes == 60){minutes = 0; hours += 1}} var uptime = document.getElementById('uptime'); uptime.innerHTML = ('Uptime: ' + hours + ' : ' + minutes + ' : ' + time)} function toggle_token() {var token = document.getElementById('token'); if(document.getElementById('token_button').innerHTML == 'Hide token'){ document.getElementById('token_button').innerHTML = 'Show token';} else{document.getElementById('token_button').innerHTML = 'Hide token'} if (token.style.display === 'none') {token.style.display = 'block';} else {token.style.display = 'none';}}</script>", "utf-8"))
+        #self.wfile.write(bytes(f"<script>var time = {get_uptime_sec()} " + "; var minutes = 0; var hours = 0; var intervalId = window.setInterval(function(){update_uptime()}, 1000); function update_uptime(){time += 1; if(time > 59){minutes += Math.round(time / 60); time = 0; if(minutes > 59){hours += Math.round(minutes / 24); minutes = 0;}} var uptime = document.getElementById('uptime'); uptime.innerHTML = ('Uptime: ' + hours + ':' + minutes + ':' + time)} function toggle_token() {var token = document.getElementById('token'); if(document.getElementById('token_button').innerHTML == 'Hide token'){ document.getElementById('token_button').innerHTML = 'Show token';} else{document.getElementById('token_button').innerHTML = 'Hide token'} if (token.style.display === 'none') {token.style.display = 'block';} else {token.style.display = 'none';}}</script>", "utf-8"))
+        self.wfile.write(bytes("<script>function toggle_token() {var token = document.getElementById('token'); if(document.getElementById('token_button').innerHTML == 'Hide token'){ document.getElementById('token_button').innerHTML = 'Show token';} else{document.getElementById('token_button').innerHTML = 'Hide token'} if (token.style.display === 'none') {token.style.display = 'block';} else {token.style.display = 'none';}}</script>", "utf-8"))
         self.wfile.write(bytes("<h1>Welcome to sheepbot26</h1>", "utf-8"))
         self.wfile.write(bytes(f"<main>", "utf-8"))
-        self.wfile.write(bytes(f"<p id = 'uptime'>Uptime: 0 : 0 : 0</p>", "utf-8"))
+        self.wfile.write(bytes(f"<p id = 'uptime'>Uptime: {get_uptime()}</p>", "utf-8"))
         self.wfile.write(bytes(f"<p id = 'token'>Token: {token}</p> <button onclick='toggle_token()'><label id = 'token_button'>Hide token</label></button>", "utf-8"))
         self.wfile.write(bytes(f"</main>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
--- sheepbot26.py
+++ sheepbot26.py
@@ -7,11 +7,14 @@ from discord.ext import commands
 from os import system, name
 import datetime
 import subprocess
+import yt_dlp
+import asyncio
 bot = commands.Bot(command_prefix=".", intents = discord.Intents.all())
 gamename = ""
 startTime = 0
 sus1 = ["is sus", "was ejected", "is so sus that he got sent out to space", "is the sussiest person alive"]
 insults = ["is trash", "sucks", "is stupid", "is the dumbest person alive", "is the fattest person alive", "bro your so dogwater noone would ever want you on there team", "is even more trash than the trash i took out last night", "was a accident", "had a brick drop on their head when they were born", "is a chicken", "is fat"]
+songs = []
 dog1 = [
     "is dogwater",
     "bro your so dogwater noone would ever want you on there team",
@@ -24,6 +27,45 @@ dog = 0
 email = ["gmail.com", "hotmail.com", "outlook.com", "oatlocker.com", "yahoo.co.nz", "oatlocker.com", "deeznuts.com", "gmail.com", "yahoo.co.nz", "outlook.com", "gmail.com"]
 number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "f", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "b", "C", "E", "F", "G", "H", "I", "F", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+yt_dlp.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': False,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+                # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
 #, ephemeral=True
 def clear():
     if name == 'nt':
@@ -85,7 +127,7 @@ async def heck(interaction: discord.Interaction, who: str):
     number = number + random.choice(letter)
     number = number + random.choice(letter)
     email1 = random.choice(email)
-    heck_embed=discord.Embed(title=f"Hecked {who}", color=discord.Color.red())
+    heck_embed=discord.Embed(title=f"Hecked {who}", color=discord.Color.blue())
     heck_embed.add_field(name='Email', value=f"{who}{number}@{email1}", inline=True)
     heck_embed.add_field(name='Password', value=f"{letters}", inline=True)
     await interaction.response.send_message(embed=heck_embed)
@@ -93,6 +135,72 @@ async def heck(interaction: discord.Interaction, who: str):
 @bot.tree.command(name='rat')
 async def rat(interaction: discord.Interaction):
     await interaction.response.send_message("https://www.youtube.com/watch?v=vdVnnMOTe3Q")
+    
+@bot.tree.command(name='join')
+async def join(interaction: discord.Interaction):
+    if interaction.user.voice is not None:
+        vc = interaction.user.voice.channel
+        voice = await vc.connect()
+        await interaction.response.send_message("Joining voice channel")
+    else:
+        await interaction.response.defer(ephemeral=True)
+        await interaction.followup.send("You are not in a voice channel.")
+        return
+    
+@bot.tree.command(name='play')
+@app_commands.describe(song = "song")
+async def play(interaction: discord.Interaction, song: str):
+    try :
+        await interaction.response.defer(thinking = True)
+        guild = interaction.guild
+        voice_channel = guild.voice_client
+        filename = await YTDLSource.from_url(song, loop=bot.loop)
+        voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        #await interaction.response.edit_message(content='**Now playing:** {}'.format(filename))
+        await interaction.followup.send('**Now playing:** {}'.format(filename))
+    except:
+        await interaction.response.send_message("The bot is not connected to a voice channel.")
+
+@bot.tree.command(name='leave')
+async def leave(interaction: discord.Interaction):
+    guild = interaction.guild
+    vc = guild.voice_client
+
+    if vc:
+        await vc.disconnect()
+        await interaction.response.send_message('Leaving voice channel')
+    else:
+        await interaction.response.send_message("The bot isn't in the voice channel")
+        
+@bot.tree.command(name='pause')
+async def pause(interaction: discord.Interaction):
+    guild = interaction.guild
+    voice_channel = guild.voice_client
+    if voice_channel.is_playing():
+        await interaction.response.send_message("Paused the music.")
+        voice_channel.pause()
+    else:
+        await interaction.response.send_message("The bot is not playing anything at the moment.")
+        
+@bot.tree.command(name='resume')
+async def resume(interaction: discord.Interaction):
+    guild = interaction.guild
+    voice_channel = guild.voice_client
+    if voice_channel.is_playing():
+        await interaction.response.send_message("Resumed the music.")
+        await voice_channel.resume()
+    else:
+        await interaction.response.send_message("The bot is not playing anything at the moment.")
+        
+@bot.tree.command(name='stop_music')
+async def stop_music(interaction: discord.Interaction):
+    guild = interaction.guild
+    voice_channel = guild.voice_client
+    if voice_channel.is_playing():
+        await interaction.response.send_message("Stopping the music.")
+        voice_channel.stop()
+    else:
+        await interaction.response.send_message("The bot is not playing anything at the moment.")
 
 @bot.event
 async def on_ready():
@@ -106,6 +214,9 @@ async def on_ready():
     
 def get_uptime():
     return str(datetime.timedelta(seconds=int(round(time.time()-startTime))))
+
+def get_uptime_sec():
+    return (round(time.time()-startTime))
 
 def logout():
     bot.close()
